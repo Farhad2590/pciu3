117b70d5218f1202228d451c52fa351030fdf442
Farhad2590 <76680813+Farhad2590@users.noreply.github.com>
2021-05-21 20:15:35 +0600

    Add files via upload

---
+++ Assignment/src/Assignment/caici.java
@@ -0,0 +1,43 @@
+package Assignment;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class caici {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					caici window = new caici();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public caici() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
